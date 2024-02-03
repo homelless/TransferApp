5c60c91d6474893385a01679c58b93fcecb087b6
homeless <hmlssgd@gmail.com>
2024-02-03 15:59:08 +0300

    10.1

---
+++ TransferApp/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  TransferApp
+//
+//  Created by MacBookAir on 3.02.24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    //...
+    @IBOutlet var dataTextField: UITextField!
+    //...
+    
+}
+
